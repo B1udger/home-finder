19962d58e7c132fb21ce9f9d93a06281a00e567e
Iliyan Stefanov <iliqnstefanovza@gmail.com>
2024-03-20 12:03:59 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Footer } from './components/footer/footer';
+import {Header} from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Footer />
     </div>
   );
