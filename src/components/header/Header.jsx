357c3683b2ffbae988547b73bb514308446cddf4
Iliyan Stefanov <iliqnstefanovza@gmail.com>
2024-04-03 16:11:29 +0300

    logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Header.css';
+import Logo from '../../img/logo.png'; 
 
-export function Header () {
+export function Header() {
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">HomeFinder</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Container className="justify-content-center">
+        <Navbar.Brand href="#home" className="mx-auto">
+          <img
+            src={Logo}
+            height="60%"
+            alt="HomeFinder Logo"
+          />
+        </Navbar.Brand>
         </Container>
-      </Navbar>
-    </>
+      </Container>
+    </Navbar>
   );
 }
