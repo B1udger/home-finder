be3b6519df1919be42c6681357a9b72c15043963
Martin Georgiev <83757143+m-georgiev01@users.noreply.github.com>
2024-04-04 17:20:02 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Footer } from './components/footer/Footer';
-import { Header } from './components/header/Header';
 import { Main } from './components/main/Main';
+import { Layout } from './components/layout/Layout';
+import { RentalForm } from './components/rentals/rental-form/RentalForm';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        <Route exact path="/" element={<Layout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/rentals/add" element={<RentalForm />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
